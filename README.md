7d29beaa5db76cf7dd7d6b3971a81c8a69c21928
Arnav Shah <sarn248@gmail.com>
2020-10-17 09:11:15 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+# PocDoc Medical Symptoms API
+
+### Uses Infermedica's API
+
+Get Your API Key and App Id Here: https://developer.infermedica.com/
+
+Replace it [Here](https://github.com/arnavs-0/medical-api/blob/e38549336c3c863e24d137d5081cf630a99bb418/api.py#L56)
---
+++ api.py
@@ -0,0 +1,65 @@
+from flask import Flask, request
+from re import match
+from requests import get, post
+
+
+app = Flask(__name__)
+
+
+@app.route('/symptoms', methods=['GET'])
+def symptoms():
+    return {'symptoms': infermedica_symptoms}, 200
+
+
+@app.route('/diagnosis', methods=['POST'])
+def diagnosis():
+    arguments = request.json if request.json else {}
+    try:
+        assert 'gender' in arguments, 'Missing <gender> argument'
+        assert str(arguments['gender']).lower() in ['male', 'female'], '<gender> argument requires value of "male" or "female"'
+        assert 'age' in arguments, 'Missing <age> argument'
+        assert match(r'^[0-9]+$', str(arguments['age'])), '<age> argument requires integer value'
+        assert 'symptoms' in arguments, 'Missing <symptoms> argument'
+        assert isinstance(arguments['symptoms'], list), '<symptoms> argument requires list of symptoms'
+    except AssertionError as error:
+        return {'message': str(error)}, 400
+
+    body = {'sex': arguments['gender'].lower(), 'age': int(arguments['age'])}
+    symptom_ids = []
+    for experienced_symptom in arguments['symptoms']:
+        symptom_ids += [{'id': experienced_symptom, 'choice_id': 'present'}]
+    body.update({'evidence': symptom_ids})
+
+    diagnosis_request = post(base+'/diagnosis', json=body, headers=__headers).json()
+    infermedica_diagnosis = []
+    try:
+        conditions = diagnosis_request['conditions']
+        for condition in conditions:
+            infermedica_diagnosis += [{'name': condition['common_name'], 'probability': condition['probability']*100}]
+    except KeyError:
+        return {'message': 'Invalid request'}, 400
+    return {'conditions': infermedica_diagnosis}, 200
+
+
+@app.route('/')
+def home():
+    return '''
+    <h2>Endpoints</h2>
+    <ul>
+        <li>/symptoms</li>
+        <li>/diagnosis</li>
+    </ul>
+    '''
+
+
+base = 'https://api.infermedica.com/v2'
+__headers = {'App-Id': 'APP_ID', 'App-Key': 'APP_KEY'}
+
+infermedica_symptoms = []
+symptoms_request = get(base+'/symptoms', headers=__headers).json()
+
+for symptom in symptoms_request:
+    infermedica_symptoms += [{'id': symptom['id'], 'name': symptom['common_name']}]
+
+if __name__ == '__main__':
+    app.run()
